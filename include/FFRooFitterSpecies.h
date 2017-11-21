642185a44c019c8a57b41ac532bcdf6598212cf0
Dominik Werthmueller <werthmuellerd@gmail.com>
2017-11-21 16:38:13 +0000

    FFRooFitterSpecies: added model parameter members

--- include/FFRooFitterSpecies.h
+++ include/FFRooFitterSpecies.h
@@ -22,7 +22,12 @@ class FFRooFitterSpecies : public TNamed
 {
 
 protected:
-    FFRooModel* fModel;             // RooFit model
+    FFRooModel* fModel;             //! FooFit model
+    TString fModelClass;            // class name of FooFit model
+    Int_t fNModelPar;               // number of model parameters
+    TString** fModelParName;        //[fNModelPar] array of model parameter names
+    Double_t* fModelParValue;       //[fNModelPar] array of model parameter values
+    Double_t* fModelParError;       //[fNModelPar] array of model parameter errors
     Double_t fYieldInit;            // initial value of yield parameter
     Double_t fYieldFit;             // fitted value of yield parameter
     Double_t fYieldFitError;        // error of fitted value of yield parameter
@@ -31,7 +36,8 @@ protected:
 
 public:
     FFRooFitterSpecies() : TNamed(),
-                           fModel(0),
+                           fModel(0), fModelClass(""),
+                           fNModelPar(0), fModelParName(0), fModelParValue(0), fModelParError(0),
                            fYieldInit(0),
                            fYieldFit(0), fYieldFitError(0),
                            fYieldMin(0), fYieldMax(0) { }
@@ -41,6 +47,11 @@ public:
     virtual ~FFRooFitterSpecies();
 
     FFRooModel* GetModel() const { return fModel; }
+    const TString& GetModelClass() const { return fModelClass; }
+    Int_t GetNModelPar() const { return fNModelPar; }
+    const TString* GetModelParName(Int_t p) const { return fModelParName[p]; }
+    Double_t GetModelParValue(Int_t p) const { return fModelParValue[p]; }
+    Double_t GetModelParError(Int_t p) const { return fModelParError[p]; }
     Double_t GetYieldInit() const { return fYieldInit; }
     Double_t GetYieldFit() const { return fYieldFit; }
     Double_t GetYieldFitError() const { return fYieldFitError; }
@@ -50,6 +61,8 @@ public:
     void SetYield(Double_t init, Double_t min, Double_t max);
     void SetYieldFit(Double_t y) { fYieldFit = y; }
     void SetYieldFitError(Double_t e) { fYieldFitError = e; }
+
+    void UpdateModelParameters();
 
     virtual void Print(Option_t* option = "") const;
 
--- src/FFRooFitterSpecies.cxx
+++ src/FFRooFitterSpecies.cxx
@@ -24,6 +24,11 @@ FFRooFitterSpecies::FFRooFitterSpecies(const Char_t* name, const Char_t* title)
 
     // init members
     fModel = 0;
+    fModelClass = "";
+    fNModelPar = 0;
+    fModelParName = 0;
+    fModelParValue = 0;
+    fModelParError = 0;
     fYieldInit = 0;
     fYieldFit = 0;
     fYieldFitError = 0;
@@ -40,6 +45,11 @@ FFRooFitterSpecies::FFRooFitterSpecies(const Char_t* name, const Char_t* title,
 
     // init members
     fModel = model;
+    fModelClass = model->ClassName();
+    fNModelPar = 0;
+    fModelParName = 0;
+    fModelParValue = 0;
+    fModelParError = 0;
     fYieldInit = 0;
     fYieldFit = 0;
     fYieldFitError = 0;
@@ -53,6 +63,14 @@ FFRooFitterSpecies::~FFRooFitterSpecies()
     // Destructor.
 
     if (fModel) delete fModel;
+    if (fModelParName)
+    {
+        for (Int_t i = 0; i < fNModelPar; i++)
+            delete fModelParName[i];
+        delete [] fModelParName;
+    }
+    if (fModelParValue) delete [] fModelParValue;
+    if (fModelParError) delete [] fModelParError;
 }
 
 //______________________________________________________________________________
@@ -67,17 +85,64 @@ void FFRooFitterSpecies::SetYield(Double_t init, Double_t min, Double_t max)
 }
 
 //______________________________________________________________________________
+void FFRooFitterSpecies::UpdateModelParameters()
+{
+    // Update the model parameter members using the model member.
+
+    // check for model
+    if (fModel)
+    {
+        // delete old arrays
+        if (fModelParName)
+        {
+            for (Int_t i = 0; i < fNModelPar; i++)
+                delete fModelParName[i];
+            delete [] fModelParName;
+        }
+        if (fModelParValue) delete fModelParValue;
+        if (fModelParError) delete fModelParError;
+
+        // update number of parameters
+        fNModelPar = fModel->GetNPar();
+
+        // create new arrays
+        fModelParName = new TString*[fNModelPar];
+        fModelParValue = new Double_t[fNModelPar];
+        fModelParError = new Double_t[fNModelPar];
+
+        // loop over parameters
+        for (Int_t i = 0; i < fNModelPar; i++)
+        {
+            fModelParName[i] = new TString(fModel->GetParName(i));
+            fModelParValue[i] = fModel->GetParameter(i);
+            fModelParError[i] = fModel->GetParError(i);
+        }
+    }
+}
+
+//______________________________________________________________________________
 void FFRooFitterSpecies::Print(Option_t* option) const
 {
     // Print out the content of this class.
 
     printf("FFRooFitterSpecies content:\n");
-    printf("Name                      : %s\n", GetName());
-    printf("Title                     : %s\n", GetTitle());
-    printf("Initial yield             : %e\n", fYieldInit);
-    printf("Fitted yield              : %e\n", fYieldFit);
-    printf("Fitted yield error        : %e\n", fYieldFitError);
-    printf("Yield minimum             : %e\n", fYieldMin);
-    printf("Yield maximum             : %e\n", fYieldMax);
+    printf("Name                            : %s\n", GetName());
+    printf("Title                           : %s\n", GetTitle());
+    printf("Initial yield                   : %e\n", fYieldInit);
+    printf("Fitted yield                    : %e\n", fYieldFit);
+    printf("Fitted yield error              : %e\n", fYieldFitError);
+    printf("Yield minimum                   : %e\n", fYieldMin);
+    printf("Yield maximum                   : %e\n", fYieldMax);
+    printf("Model class name                : %s\n", fModelClass.Data());
+    printf("Model parameters                : %d\n", fNModelPar);
+    if (fNModelPar)
+    {
+        // loop over parameters
+        for (Int_t i = 0; i < fNModelPar; i++)
+        {
+            printf("  %-30s: %14.7e +/- %14.7e\n",
+                   fModelParName[i]->Data(), fModelParValue[i], fModelParError[i]);
+        }
+    }
 }
 
