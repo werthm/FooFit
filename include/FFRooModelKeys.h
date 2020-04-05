3bcafdc11e319eb0a735c1113a4a053a86165dcc
Dominik Werthmueller <werthmuellerd@gmail.com>
2020-04-05 15:37:23 +0200

    FFRooModelKeys: added altern. keys implement., support for shift

--- include/FFRooModelKeys.h
+++ include/FFRooModelKeys.h
@@ -15,6 +15,8 @@
 #ifndef FOOFIT_FFRooModelKeys
 #define FOOFIT_FFRooModelKeys
 
+#include "RooKeysPdf.h"
+
 #include "FFRooModel.h"
 
 class TTree;
@@ -24,6 +26,13 @@ class FFRooModelKeys : public FFRooModel
 {
 
 protected:
+    enum EKeysPdfType {
+        kUndef,
+        kRooKeys,
+        kRooNDKeys
+    };
+
+    EKeysPdfType fType;             // type of underlying pdf
     Int_t fNDim;                    // number of dimensions
     TTree* fTree;                   // event tree
     RooDataSet* fDataSet;           // RooFit dataset
@@ -31,14 +40,19 @@ protected:
     Double_t fRho;                  // rho parameter for RooNDKeysPdf
     Int_t fNSigma;                  // nSigma parameter for RooNDKeysPdf
     Bool_t fRotate;                 // rotate parameter for RooNDKeysPdf
+    RooKeysPdf::Mirror fMirror;     // mirror parameter for RooKeysPdf
 
 public:
     FFRooModelKeys() : FFRooModel(),
+                       fType(kUndef),
                        fNDim(0),
                        fTree(0), fDataSet(0),
-                       fOpt(""), fRho(0), fNSigma(0), fRotate(kTRUE) { }
+                       fOpt(""), fRho(0), fNSigma(0), fRotate(kTRUE),
+                       fMirror(RooKeysPdf::NoMirror) { }
     FFRooModelKeys(const Char_t* name, const Char_t* title, Int_t nDim, TTree* tree,
                    const Char_t* opt = "a", Double_t rho = 1, Int_t nSigma = 3, Bool_t rotate = kTRUE);
+    FFRooModelKeys(const Char_t* name, const Char_t* title, TTree* tree, Bool_t addShiftPar = kFALSE,
+                   RooKeysPdf::Mirror mirror = RooKeysPdf::NoMirror, Double_t rho = 1);
     virtual ~FFRooModelKeys();
 
     virtual void BuildModel(RooAbsReal** vars);
--- src/FFRooModelKeys.cxx
+++ src/FFRooModelKeys.cxx
@@ -15,6 +15,7 @@
 #include "TTree.h"
 #include "RooDataSet.h"
 #include "RooNDKeysPdf.h"
+#include "RooRealVar.h"
 
 #include "FFRooModelKeys.h"
 
@@ -25,10 +26,11 @@ FFRooModelKeys::FFRooModelKeys(const Char_t* name, const Char_t* title, Int_t nD
                                const Char_t* opt, Double_t rho, Int_t nSigma, Bool_t rotate)
     : FFRooModel(name, title, 0)
 {
-    // Constructor.
+    // Constructor for 'nDim'-dim. pdf using RooNDKeysPdf.
     // See RooNDKeysPdf for meaning of parameters.
 
     // init members
+    fType = kRooNDKeys;
     fNDim = nDim;
     fTree = tree;
     fDataSet = 0;
@@ -36,6 +38,33 @@ FFRooModelKeys::FFRooModelKeys(const Char_t* name, const Char_t* title, Int_t nD
     fRho = rho;
     fNSigma = nSigma;
     fRotate = rotate;
+    fMirror = RooKeysPdf::NoMirror;
+}
+
+//______________________________________________________________________________
+FFRooModelKeys::FFRooModelKeys(const Char_t* name, const Char_t* title, TTree* tree, Bool_t addShiftPar,
+                               RooKeysPdf::Mirror mirror, Double_t rho)
+    : FFRooModel(name, title, addShiftPar ? 1 : 0)
+{
+    // Constructor for 1-dim. pdf using RooKeysPdf.
+    // See RooKeysPdf for meaning of parameters.
+
+    fType = kRooKeys;
+    fNDim = 1;
+    fTree = tree;
+    fDataSet = 0;
+    fOpt = "";
+    fRho = rho;
+    fNSigma = 0;
+    fRotate = kFALSE;
+    fMirror = mirror;
+
+    // add shift parameters
+    if (addShiftPar)
+    {
+        TString tmp = TString::Format("%s_Shift", GetName());
+        AddParameter(0, tmp.Data(), tmp.Data());
+    }
 }
 
 //______________________________________________________________________________
@@ -66,7 +95,31 @@ void FFRooModelKeys::BuildModel(RooAbsReal** vars)
     // create RooFit dataset
     if (fDataSet)
         delete fDataSet;
-    fDataSet = new RooDataSet(fTree->GetName(), fTree->GetTitle(), varSet, RooFit::Import(*fTree));
+
+    // extend range if shift parameter is present
+    if (fNPar)
+    {
+        // get variable/parameter info
+        RooRealVar* r = (RooRealVar*)vars[0];
+        RooRealVar* p = (RooRealVar*)fPar[0];
+        Double_t min = r->getMin();
+        Double_t max = r->getMax();
+
+        // adjust variable range
+        r->setMin(TMath::Min(min, TMath::Min(min - p->getMin(), min - p->getMax())));
+        r->setMax(TMath::Max(max, TMath::Max(max - p->getMin(), max - p->getMax())));
+
+        // create data set
+        fDataSet = new RooDataSet(fTree->GetName(), fTree->GetTitle(), varSet, RooFit::Import(*fTree));
+
+        // restore variable range
+        r->setMin(min);
+        r->setMax(max);
+    }
+    else
+    {
+        fDataSet = new RooDataSet(fTree->GetName(), fTree->GetTitle(), varSet, RooFit::Import(*fTree));
+    }
 
     // user info
     Int_t nEntries = fDataSet->numEntries();
@@ -76,6 +129,33 @@ void FFRooModelKeys::BuildModel(RooAbsReal** vars)
     // create the model pdf
     if (fPdf)
         delete fPdf;
-    fPdf = new RooNDKeysPdf(GetName(), GetTitle(), varList, *fDataSet, fOpt.Data(), fRho, fNSigma, fRotate);
+
+    // select pdf implementation
+    if (fType == kRooNDKeys)
+    {
+        fPdf = new RooNDKeysPdf(GetName(), GetTitle(), varList, *fDataSet, fOpt.Data(), fRho, fNSigma, fRotate);
+    }
+    else if (fType == kRooKeys)
+    {
+        // check for shift parameter
+        if (fNPar)
+        {
+            // add shift transformation
+            TString tmp = TString::Format("%s - %s", vars[0]->GetName(), GetPar(0)->GetName());
+            RooAbsReal* trans = new RooFormulaVar(TString::Format("Trans_Shift_%s_Var_%s", GetName(), vars[0]->GetName()).Data(),
+                                                  tmp.Data(), RooArgSet(*vars[0], *GetPar(0)));
+            AddVarTrans(trans);
+            fPdf = new RooKeysPdf(GetName(), GetTitle(), *trans, *((RooRealVar*)(vars[0])), *fDataSet, fMirror, fRho);
+        }
+        else
+        {
+            fPdf = new RooKeysPdf(GetName(), GetTitle(), *vars[0], *fDataSet, fMirror, fRho);
+        }
+    }
+    else
+    {
+        fPdf = 0;
+        Error("BuildModel", "Unknown keys pdf type!");
+    }
 }
 
